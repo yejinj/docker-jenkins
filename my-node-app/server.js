1580c5b9e7aededd7eb7bb33ee95dfaea5699ded
yejinj <sitting077@gmail.com>
2025-03-07 14:43:19 +0900

    chore: comment deleted

--- my-node-app/server.js
+++ my-node-app/server.js
@@ -6,28 +6,23 @@ const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
-// 미들웨어 설정
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// MongoDB 연결
 mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-.then(() => console.log('MongoDB 연결 성공'))
-.catch((err) => console.error('MongoDB 연결 실패:', err));
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
-// 라우트 설정
 app.use('/api/users', userRoutes);
 
-// 테스트 라우트
 app.get('/', (req, res) => {
-    res.json({ message: '서버가 정상적으로 실행중입니다.' });
+    res.json({ message: 'Server is running' });
 });
 
-// 서버 시작
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`서버가 ${PORT} 포트에서 실행중입니다.`);
+    console.log(`Server is running at ${PORT} port.`);
 }); 
